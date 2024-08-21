81f1aea4490bce655f97027f97d3d7a83596475e
Luis Henrique Cardoso <127408909+xXLuisHenriqueXx@users.noreply.github.com>
2024-08-21 19:17:29 -0300

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
+const cors = require('cors');
 const express = require('express');
 const router = require('./router');
 
 const app = express();
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(router);
